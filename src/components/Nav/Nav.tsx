72f2edb3cdf1c55f6680ba7b1aa2bc996d226b9d
Shakir <shakirrahman81@gmail.com>
2025-02-21 12:19:25 -0500

    Adds Homepage layout

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import './styles.scss';
+
+const Nav: React.FC = ({}) => {
+  return (
+    <div className='navbar'>
+    </div>
+  );
+};
+
+export default Nav;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,35 +1,16 @@
 import React from 'react';
-import Button from '../../components/Button/Button';
 import styles from './styles.module.scss';
 import { useUser } from '../../context/UserContext';
 import AnimatedBackground from '../../assets/background/AnimatedBackground';
+import Nav from '../../components/Nav/Nav';
 
 const Home: React.FC = () => {
   const { username } = useUser();
   return (
     <div className={styles.container}>
+      <Nav/>
+      <div className={styles.mainContent}/>
       <AnimatedBackground className={styles.bg}/>
-      <div className={styles.header}>
-        <div className={styles.title}>
-            <p className={styles.title1}>WELCOME,</p>
-            <p className={styles.title2}>{username}</p>
-        </div>
-      </div>
-      <div className={styles.body}>
-        <div className={styles.stats}>
-          
-        </div>
-        <div className={styles.nav}>
-          <Button
-            text='Sign up!'
-            onClick={() => console.log('Works')}
-          />
-          <Button
-            text='Sign up!'
-            onClick={() => console.log('Works')}
-          />
-        </div>
-      </div>
     </div>
   );
 };
